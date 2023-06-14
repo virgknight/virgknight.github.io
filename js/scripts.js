0cbe4e5973c94512033481bba7cc231d02122c64
virgknight <virginiaknight888@gmail.com>
2023-06-13 21:05:56 -0400

    update resume and location

--- js/scripts.js
+++ js/scripts.js
@@ -98,7 +98,7 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    const navBarBrandText = ["name: 'Virginia Knight'", "occupation: 'Software Developer'", "location: 'Scottsdale, AZ'"];
+    const navBarBrandText = ["name: 'Virginia Knight'", "occupation: 'Software Developer'", "location: 'New York, NY'"];
     const navBrandName = document.body.querySelector('.nav-brand-name');
     new TxtType(navBrandName, navBarBrandText);
 
